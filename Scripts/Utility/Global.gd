3ddb9a47a2120769a640fc1a49aa0f821c5fc36c
pat <74840936+patllamas@users.noreply.github.com>
2025-03-23 18:13:30 -0400

    added global preload and global vars

---
+++ Scripts/Utility/Global.gd
@@ -0,0 +1,21 @@
+extends Node
+
+# hold currency here; hold as float, show as int
+var honey_count : float = 0
+var pollen_count : float = 0
+var diamond_count : float = 0
+
+# everytime a player upgrades, increase cost by this much
+func new_cost(base_cost : float, structure_level : int):
+	var new_cost : float
+	
+	#this makes it easier to purchase upgrades at a lower levels
+	if structure_level <= 15:
+		new_cost = floorf((5 + structure_level) * pow(1.06, structure_level - 1))
+	else:
+		new_cost = floorf((20 + structure_level) * pow(1.06, structure_level - 1))
+	
+	return new_cost
+
+# hold levels of each structure here
+var level_big_tree = 1
---
+++ Scripts/Utility/GlobalPreloads.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var ResourceLabelParticle : Object = preload("res://Scenes/UI/resource_label_particle.tscn")
