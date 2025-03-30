91d620b9696a1896746320803f153db41b20c1f0
pat <74840936+patllamas@users.noreply.github.com>
2025-03-30 19:13:51 -0400

    added tabs to change upgrades

--- Scripts/UI/hud.gd
+++ Scripts/UI/hud.gd
@@ -1,5 +1,6 @@
 extends CanvasLayer
 @onready var honey_label: Label = $HoneyLabel
+@onready var tab_container: TabContainer = $UpgradeContainer/TabContainer
 
 #TODO Optimize?
 # do we say fuggit and update the text every frame?
@@ -7,3 +8,19 @@ extends CanvasLayer
 
 func _physics_process(_delta: float) -> void:
 	honey_label.text = str(roundi(Global.honey_count))
+
+
+func _on_base_tree_tab_btn_pressed() -> void:
+	tab_container.current_tab = 0
+
+
+func _on_bears_tab_btn_pressed() -> void:
+	tab_container.current_tab = 1
+
+
+func _on_picnic_tab_btn_pressed() -> void:
+	tab_container.current_tab = 2
+
+
+func _on_prestige_tab_btn_pressed() -> void:
+	tab_container.current_tab = 3
